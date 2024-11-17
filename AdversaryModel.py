14c27716ec89d489d9e2ff88227e4682ade97023
Stellarone60 <sarah.carley@hotmail.com>
2024-11-16 17:30:28 -0800

    Changed to BERT

--- AdversaryModel.py
+++ AdversaryModel.py
@@ -1,4 +1,10 @@
 import torch
-from transformers import GPT2Tokenizer, GPT2LMHeadModel, Trainer, TrainingArguments
+from transformers import BertTokenizer, BertForMaskedLM, Trainer, TrainingArguments 
 from datasets import load_dataset
+
+model_name = 'bert-base-uncased'#name of the model
+model = BertForMaskedLM.from_pretrained(model_name)#model
+tok = BertTokenizer.from_pretrained(model_name)#tokenizer
+
+data = load_dataset("Plishing_validation_emails.csv")
 
