efac0c9bb06a32358f06f851e281ba870162c5b9
Stellarone60 <sarah.carley@hotmail.com>
2024-12-01 20:08:55 -0800

    Train, dang it

--- AI_Model.py
+++ AI_Model.py
@@ -13,12 +13,76 @@ from sklearn.model_selection import train_test_split
 
 
 dataset = pd.read_csv('Phishing_validation_emails.csv')
-model_path = "google-bert/bert-base-uncased"
+
+#edits
+train_df, test_df = train_test_split(dataset)
+train_dataset = Dataset.from_pandas(train_df)
+test_dataset = Dataset.from_pandas(test_df)
+dataset_dict = DatasetDict({"train": train_dataset, "test": test_dataset})
+#edits end
+
+
+#model_path = "google-bert/bert-base-uncased"
+
+model_path = "bert-base-uncased"
 tokenizer = AutoTokenizer.from_pretrained(model_path)
 id2label = {0: "Safe Email", 1: "Phishing Email"}
 label2id = {"Safe Email": 0, "Phishing Email": 1}
-model = AutoModelForSequenceClassification.from_pretrained(model_path, num_labels = 2, idl2label=id2label, idlabel2id=label2id)
+model = AutoModelForSequenceClassification.from_pretrained(model_path, num_labels = 2, id2label=id2label, label2id=label2id)
 
+for name, param in model.base_model.named_parameters():
+    param.requires_grad = False
+
+for name, param in model.base_model.named_parameters():
+    if "pooler" in name:
+        param.requires_grad = True
+    
+def preprocess_function (examples):
+    return tokenizer(examples["Email Text"], truncation=True,padding=True)
+
+tokenized_data = dataset_dict.map(preprocess_function,batched=True)
+
+data_collator = DataCollatorWithPadding(tokenizer=tokenizer)
+
+accuracy = evaluate.load("accuracy")
+auc_score = evaluate.load("roc_auc")
+
+def compute_metrics(eval_pred):
+    predictions, labels = eval_pred
+    probabilities = np.exp(predictions) / np.exp(predictions).sum(-1, keepdims = True)
+    positive_class_probs = probabilities[:1]
+    auc=np.round(auc_score.compute(prediction_scores=positive_class_probs, references=labels)['roc_auc'],3)
+    predicted_classes = np.argmax(predictions, axis=1)
+    acc=np.round(accuracy.compute(predictions=predicted_classes, references=labels)['accuracy'],3)
+    return{"Accuracy": acc, "AUC": auc}
+
+
+lr = 2e-4
+batch_size = 8
+num_epochs = 10
+training_args = TrainingArguments(
+    output_dir = "bert-phishing-classifier_teacher",
+    learning_rate=lr,
+    per_device_train_batch_size=batch_size,
+    per_device_eval_batch_size=batch_size,
+    num_train_epochs=num_epochs,
+    logging_strategy="epoch",
+    eval_strategy="epoch",
+    save_strategy="epoch",
+    load_best_model_at_end=True,
+)
+
+trainer = Trainer(
+    model=model,
+    args = training_args,
+    train_dataset=tokenized_data["train"],
+    eval_dataset=tokenized_data["test"],
+    tokenizer = tokenizer,
+    data_collator=data_collator,
+    compute_metrics=compute_metrics,
+)
+
+trainer.train()
 
 """
 from transformers import GPT2Tokenizer, GPT2ForSequenceClassification, Trainer, TrainingArguments
